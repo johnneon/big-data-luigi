5715dec9c2131f765ba7d8a1ee1856aca2ef62ce
johnneon <neon.1598@mail.ru>
2024-11-17 14:40:56 +0300

    feat: process tables

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import requests
 import tarfile
 import gzip
 import shutil
+import io
+import pandas as pd
 
 
 class DownloadDataset(luigi.Task):
@@ -17,6 +19,7 @@ class DownloadDataset(luigi.Task):
         url = f"https://www.ncbi.nlm.nih.gov/geo/download/?acc={self.dataset_name}&format=file"
         os.makedirs(self.output_dir, exist_ok=True)
         response = requests.get(url, stream=True)
+
         with open(self.output().path, "wb") as f:
             f.write(response.content)
 
@@ -34,15 +37,72 @@ class ExtractArchive(luigi.Task):
     def run(self):
         extracted_dir = self.output().path
         os.makedirs(extracted_dir, exist_ok=True)
+
         with tarfile.open(self.input().path, "r") as tar:
             tar.extractall(extracted_dir)
+
         for file in os.listdir(extracted_dir):
             if file.endswith(".gz"):
                 filepath = os.path.join(extracted_dir, file)
+
                 with gzip.open(filepath, "rb") as f_in:
+
                     with open(filepath[:-3], "wb") as f_out:
                         shutil.copyfileobj(f_in, f_out)
+
                 os.remove(filepath)
+
+
+class ProcessTables(luigi.Task):
+    dataset_name = luigi.Parameter()
+    output_dir = luigi.Parameter(default="data")
+
+    def requires(self):
+        return ExtractArchive(self.dataset_name, self.output_dir)
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(self.output_dir, self.dataset_name, "processed"))
+
+    def run(self):
+        processed_dir = self.output().path
+        os.makedirs(processed_dir, exist_ok=True)
+        extracted_dir = self.input().path
+
+        for file in os.listdir(extracted_dir):
+            if file.endswith(".txt"):
+                input_path = os.path.join(extracted_dir, file)
+                dfs = {}
+                with open(input_path, "r") as f:
+                    write_key = None
+                    fio = io.StringIO()
+                    for line in f.readlines():
+                        if line.startswith('['):
+                            if write_key:
+                                fio.seek(0)
+                                header = None if write_key == 'Heading' else 'infer'
+                                dfs[write_key] = pd.read_csv(fio, sep='\t', header=header)
+                            fio = io.StringIO()
+                            write_key = line.strip('[]\n')
+                            continue
+                        if write_key:
+                            fio.write(line)
+                    if write_key:
+                        fio.seek(0)
+                        dfs[write_key] = pd.read_csv(fio, sep='\t')
+
+                for key, df in dfs.items():
+                    output_file = os.path.join(processed_dir, f"{key}.tsv")
+                    df.to_csv(output_file, sep='\t', index=False)
+
+                if "Probes" in dfs:
+                    reduced_df = dfs["Probes"].drop(columns=[
+                        "Definition", "Ontology_Component", "Ontology_Process",
+                        "Ontology_Function", "Synonyms", "Obsolete_Probe_Id", "Probe_Sequence"
+                    ], errors="ignore")
+                    reduced_file = os.path.join(processed_dir, "Probes_reduced.tsv")
+                    reduced_df.to_csv(reduced_file, sep='\t', index=False)
+
+                os.remove(input_path)
 
 
 if __name__ == "__main__":
