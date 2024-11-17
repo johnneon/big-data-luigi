65061d4788ae60e2db2ea8ef706007df1dfbf571
johnneon <neon.1598@mail.ru>
2024-11-17 15:03:55 +0300

    docs: doc project

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import pandas as pd
 
 
 class DownloadDataset(luigi.Task):
+    """
+        Загрузка датасета из источника
+    """
     dataset_name = luigi.Parameter()
     output_dir = luigi.Parameter(default="data")
 
@@ -18,17 +21,23 @@ class DownloadDataset(luigi.Task):
     def run(self):
         url = f"https://www.ncbi.nlm.nih.gov/geo/download/?acc={self.dataset_name}&format=file"
         os.makedirs(self.output_dir, exist_ok=True)
+        # Загружаем датасет
         response = requests.get(url, stream=True)
 
+        # Сохраняем его в output_dir
         with open(self.output().path, "wb") as f:
             f.write(response.content)
 
 
 class ExtractArchive(luigi.Task):
+    """
+        Подготовка загруженных данных для дальнейшей обработки
+    """
     dataset_name = luigi.Parameter()
     output_dir = luigi.Parameter(default="data")
 
     def requires(self):
+        # Указываем что эта задача зависит от скачивания архива
         return DownloadDataset(self.dataset_name, self.output_dir)
 
     def output(self):
@@ -38,9 +47,11 @@ class ExtractArchive(luigi.Task):
         extracted_dir = self.output().path
         os.makedirs(extracted_dir, exist_ok=True)
 
+        # Разархивация основного архива
         with tarfile.open(self.input().path, "r") as tar:
             tar.extractall(extracted_dir)
 
+        # Распаковываем каждый .gz файл в этой папке
         for file in os.listdir(extracted_dir):
             if file.endswith(".gz"):
                 filepath = os.path.join(extracted_dir, file)
@@ -50,14 +61,19 @@ class ExtractArchive(luigi.Task):
                     with open(filepath[:-3], "wb") as f_out:
                         shutil.copyfileobj(f_in, f_out)
 
+                # Удаляем оригинальный .gz файл после распаковки
                 os.remove(filepath)
 
 
 class ProcessTables(luigi.Task):
+    """
+        Обработка данных в удобное для нас представление
+    """
     dataset_name = luigi.Parameter()
     output_dir = luigi.Parameter(default="data")
 
     def requires(self):
+        # Указываем что эта задача зависит от распаковки архива
         return ExtractArchive(self.dataset_name, self.output_dir)
 
     def output(self):
@@ -94,6 +110,7 @@ class ProcessTables(luigi.Task):
                     output_file = os.path.join(processed_dir, f"{key}.tsv")
                     df.to_csv(output_file, sep='\t', index=False)
 
+                # Если среди таблиц есть "Probes", создаем её урезанную версию
                 if "Probes" in dfs:
                     reduced_df = dfs["Probes"].drop(columns=[
                         "Definition", "Ontology_Component", "Ontology_Process",
@@ -102,17 +119,23 @@ class ProcessTables(luigi.Task):
                     reduced_file = os.path.join(processed_dir, "Probes_reduced.tsv")
                     reduced_df.to_csv(reduced_file, sep='\t', index=False)
 
+                # Удаляем оригинальный текстовый файл после обработки
                 os.remove(input_path)
 
 
-class CleanupTask(luigi.Task):
+class FinalTask(luigi.Task):
+    """
+        Удаление промежуточных файлов
+    """
     dataset_name = luigi.Parameter()
     output_dir = luigi.Parameter(default="data")
 
     def requires(self):
+        # Указываем что эта задача зависит от обработки таблиц
         return ProcessTables(self.dataset_name, self.output_dir)
 
     def run(self):
+        # Удаляем папку с промежуточными распакованными данными
         extracted_dir = os.path.join(self.output_dir, self.dataset_name, "extracted")
         if os.path.exists(extracted_dir):
             shutil.rmtree(extracted_dir)
